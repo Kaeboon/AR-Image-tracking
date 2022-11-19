170f1522ca6bce56d78564b7d5a97b86c8c94e0e
Ho Kae Boon <69498161+Kaeboon@users.noreply.github.com>
2022-11-20 03:45:13 +0800

    Add files via upload

---
+++ Scripts/SpawnObjectOnPlaneButton.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+//script from DevEnabled https://www.youtube.com/watch?v=phDbAMYVkzw&list=PL9z3tc0RL6Z4WenhJiJieCcrPVNxYszod&index=5
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class SpawnObjectOnPlaneButton : MonoBehaviour
+{
+    private ARRaycastManager raycastManager;
+    private GameObject spawnedObject;
+    private List<GameObject> placedPrefabList = new List<GameObject>();
+
+    [SerializeField] //for exposing variable in the editor without making it a public variable
+    private int maxPrefabSpawnCount = 0;
+    private int placedPrefabCount;
+
+    [SerializeField]
+    private GameObject placeablePrefab;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    private void Update()
+    {
+        if(!TryGetTouchPosition(out Vector2 touchPosition))
+        {
+            return;
+        }
+
+        if(raycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = s_Hits[0].pose;
+
+                if (placedPrefabCount < maxPrefabSpawnCount)
+                {
+                    SpawnPrefab(hitPose);
+                }
+        }
+    }
+    
+    public void SetPrefabType(GameObject prefabType)
+    {
+        placeablePrefab = prefabType;
+    }
+
+    private void SpawnPrefab(Pose hitPose)
+    {
+        spawnedObject = Instantiate(placeablePrefab, hitPose.position, hitPose.rotation);
+        placedPrefabList.Add(spawnedObject);
+        placedPrefabCount++;
+    }
+}
